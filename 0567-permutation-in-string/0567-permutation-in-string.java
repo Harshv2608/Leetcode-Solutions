d18696dc0c9c0e9eb3ba0843a2eba55eeba5363e
Harsh Vardhan <146832400+Harshv2608@users.noreply.github.com>
2026-06-08 14:50:18 +0530

    Time: 8 ms (53.51%), Space: 43.6 MB (96.51%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length()){
+            return false;
+        }
+        int[] s1freq=new int[26];
+        int[] s2freq=new int[26];
+        for(int i=0;i<s1.length();i++){
+            s1freq[s1.charAt(i)-'a']++;
+            s2freq[s2.charAt(i)-'a']++;
+        }
+        if(Arrays.equals(s1freq,s2freq)){
+            return true;
+        }
+        int l=0;
+        for(int r=s1.length();r<s2.length();r++){
+            s2freq[s2.charAt(r)-'a']++;
+            s2freq[s2.charAt(l)-'a']--;
+            l++;
+            if(Arrays.equals(s1freq,s2freq))return true;
+        }
+        return false;
+    }
+}
