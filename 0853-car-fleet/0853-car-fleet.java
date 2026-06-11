2f8839fb49446dcf9f5270a295db84e3ca1764fa
Harsh Vardhan <146832400+Harshv2608@users.noreply.github.com>
2026-06-11 17:06:34 +0530

    Time: 85 ms (49.61%), Space: 95.9 MB (71.41%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -1,18 +1,19 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
         int n=position.length;
-        int[][] pair=new int[n][2];
+        double[][] pair=new double[n][2];
         for(int i=0;i<n;i++){
             pair[i][0]=position[i];
-            pair[i][1]=speed[i];
+            pair[i][1]=(double)(target-position[i])/speed[i];
         }
-        Stack<Double> st=new Stack<>();
-        Arrays.sort(pair,(a,b)-> Integer.compare(b[0],a[0]));
-        for(int[] p : pair){
-            st.push((double)(target-p[0])/p[1]);
-            if(st.size()>=2 && st.peek()<=st.get(st.size()-2)){
-                st.pop();
+        Arrays.sort(pair,(a,b)-> Double.compare(b[0],a[0]));
+        int count=0;
+        double prev=0;
+        for(double[] p : pair){
+            if(p[1]>prev){
+                count++;
+                prev=p[1];
             }
-        }return st.size();
+        }return count;
     }
 }
