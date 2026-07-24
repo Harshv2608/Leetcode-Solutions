ebd086cdac3745a9b04b6a51ed554e8a7c966d3c
Harsh Vardhan <146832400+Harshv2608@users.noreply.github.com>
2026-07-24 17:11:25 +0530

    Time: 26 ms (15.94%), Space: 47.6 MB (87.69%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,26 +1,29 @@
 class Solution {
-    public long calculatehours(int[] piles,int hour){
-        long total=0;
-        for(int i: piles){
-            total+=Math.ceil((double)i/hour);
+    public int calculate(int[] piles,int n,int k){
+        int total=0;
+        for(int i=0;i<n;i++){
+            total+=Math.ceil((double)piles[i]/(double)k);
         }
         return total;
     }
     public int minEatingSpeed(int[] piles, int h) {
-        int max=Arrays.stream(piles).max().getAsInt();
+        int n=piles.length;
+        int max=0;
+        for(int i=0;i<n;i++){
+            max=Math.max(max,piles[i]);
+        }
         int low=1;
         int high=max;
-        int ans=max;
+        int ans=high;
         while(low<=high){
-            int mid=(low+high)/2;
-            long hours=calculatehours(piles,mid);
-            if(hours<=h){
+            int mid=low+(high-low)/2;
+            int reqtime=calculate(piles,n,mid);
+            if(reqtime<=h){
                 ans=mid;
                 high=mid-1;
             }else{
                 low=mid+1;
             }
-        }
-        return ans;
+        }return ans;
     }
 }
