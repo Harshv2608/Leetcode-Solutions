58001cdf74959ff63fea9759e117a04337cb972c
Harsh Vardhan <146832400+Harshv2608@users.noreply.github.com>
2026-08-08 23:20:33 +0530

    Time: 0 ms (100%), Space: 45 MB (23.19%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n=cost.length;
+        int prev=0;
+        int prev2=Integer.MAX_VALUE;
+        for(int i=n-1;i>=0;i--){
+            int curr=cost[i] + Math.min(prev,prev2);
+            prev2=prev;
+            prev=curr;
+        }
+        return Math.min(prev2,prev);
+    }
+}
